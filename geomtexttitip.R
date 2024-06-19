da21a40fb6159a973bf631dfd71f0f1446232815
taufiqsyahrial <taufiqsyahrial1234@gmail.com>
2024-06-20 00:19:30 +0100

    Delete geomtexttitip.R

--- geomtexttitip.R
+++
@@ -1,3 +0,0 @@
-geom_text(aes(label= round(percentage*100, digits=2)), position=position_dodge(width=0.9), vjust=1) 
-  
-  geom_text(aes(label= round(percentage*100, digits=2)), position=position_dodge(width=0.9), vjust=1) 
